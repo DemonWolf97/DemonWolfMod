9645f212f014afee31339ad9014f8664618fcbf0
DemonWolf <demonwolfyt@gmail.com>
2015-08-10 17:20:36 -0500

    First Commit

--- src/main/java/com/demonwolfdev/demonwolfmod/init/blocks/DemonWolfBlockInfoToolTip.java
+++
@@ -1,6 +0,0 @@
-package com.demonwolfdev.demonwolfmod.init.blocks;
-
-public class DemonWolfBlockInfoToolTip extends DemonWolfBlocks{
-	
-
-}
--- src/main/java/com/demonwolfdev/demonwolfmod/proxy/CommonProxy.java
+++
@@ -1,6 +0,0 @@
-package com.demonwolfdev.demonwolfmod.proxy;
-
-public class CommonProxy {
-	public void registerRenders() {
-	}
-}
